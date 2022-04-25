630c12599659c2113fb9aaa26c1df9bfe574dfae
Birhanu Gebisa <birhanugebisa@gmail.com>
2022-04-25 10:47:33 +0300

    update the hello world to introduction

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,10 @@
 print("Hello world of python")
+class person:
+    def __init__(self, name, age, rank):
+        self.name= name
+        self.age=age
+        self.rank=rank
+    def get_data(self):
+        print("Hello my name is " + self.name + "i am " + str(self.age) + " years old. " + "i have msc in " + self.rank)
+coll=person("Birhanu Gebisa ", 26 , "Computer science and engineering")
+coll.get_data()
